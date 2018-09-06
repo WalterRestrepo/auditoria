cbc2372fe233a337c7371981f167caa8817d959a
Walter Restrepo <jhon.restrepoc@comunidad.iush.edu.co>
2018-09-05 21:22:15 -0500

    Clase 20180905

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //console.log("Console test");
+//codepen
+//node server.js ->Para correr
 var express = require("express");
 var app = express();
 var path = require("path");
+var mysql = require("mysql");
 
 app.get('/', function (req, res) {
     res.sendFile(
@@ -11,12 +14,49 @@ app.get('/', function (req, res) {
     );
 });
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_auditoria"
+});
+
 app.get('/crearEmpresa', function (req, res) {
     res.sendFile(
         path.join(
             __dirname + '/src/views/crearEmpresa.html'
         )
     );
+});
+
+app.get('/usuario/get/:id', function (req, res) {
+    var id = req.params.id;
+    con.connect(function (err) {
+        if (err) throw err;
+        query = "SELECT * FROM usuario WHERE id = :id";
+        query = query.replace(":id", id);
+        con.query(query, function (err, result, fields) {
+            if (err) throw err;
+            console.log(err);
+            res.json(result);
+        });
+    });
+    
+});
+
+app.get('/usuario/save/:nombre', function (req, res) {
+    var nombre = req.params.nombre;
+    if(nombre == "" || nombre == undefined)
+    con.connect(function (err) {
+        if (err) throw err;
+        query = "SELECT * FROM usuario WHERE id = :id";
+        query = query.replace(":id", id);
+        con.query(query, function (err, result, fields) {
+            if (err) throw err;
+            console.log(err);
+            res.json(result);
+        });
+    });
 });
 
 //app.use(express.static(__dirname + ));
