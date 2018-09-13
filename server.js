b8ce0421d9531ed545ae270db9ffd141e220f225
Walter Restrepo <jhon.restrepoc@comunidad.iush.edu.co>
2018-09-12 21:26:49 -0500

    20180912

--- server.js
+++ server.js
@@ -122,6 +122,57 @@ app.route('/signup')
                 res.redirect('/signup');
             });
     });
+app.get('/auditar', function (req, res) {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/auditar.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+app.post('/auditoria/crear', function (req, res) {
+    var empresa = req.body.empresa;
+    var fecha = req.body.fecha;
+    var norma = req.body.norma;
+    var id_usuario = req.body.id_usuario;
+    con.query("INSERT INTO auditoria (id, empresa, norma, fecha, auditor) VALUES (?, ?, ?, ?, ?);",
+        [null, empresa, norma, fecha, id_usuario], function (err, result, fields) {
+            if (err) throw err;
+            if (result.affectedRows >= 1) {
+                res.json({ insertID: result.insertId });
+            } else {
+                res.json({ insertID: -1 });
+            }
+        });
+});
+app.get('/id_usuario/get', function (req, res) {
+    res.json(1);
+});
+
+app.get('/empresa/get', function (req, res) {
+    con.query("SELECT * FROM empresa", function (err, result, fields) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+app.get('/norma/get', function (req, res) {
+    con.query("SELECT * FROM norma", function (err, result, fields) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+app.listen(3000, function () {
+    console.log("Funciona 127.0.0.1:3000");
+});
+/*
+app.get("/", function (req, res){
+    res.send("Hola.");    
+});
+app.get("/test", function (req, res){
+    res.send("Test.");    
+});
+app.listen(3000, function (){console.log("Funciona");});*/
+
+
 // app.get('/norma', (req, res) => {
 //     if (req.session.user && req.cookies.user_sid) {
 //         res.sendFile(__dirname + '/public/norma.html');
@@ -134,137 +185,108 @@ app.route('/signup')
 //app.use(express.static(__dirname + ));
 //materialize
 
+
 //NUEVO
 //auditoria
-app.get('/auditoria/get', function (req, res) {
-    con.query("SELECT * FROM auditoria", function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/auditoria/get/:id', function (req, res) {
-    var id = req.params.id;
-    con.query("SELECT * FROM auditoria WHERE id = ?", [id], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/auditoria/save/:empresa/:pregunta/:respuesta/:norma/:auditor', function (req, res) {
-    var empresa = req.params.empresa;
-    var pregunta = req.params.pregunta;
-    var respuesta = req.params.respuesta;
-    var norma = req.params.norma;
-    var auditor = req.params.auditor;
-    con.query("INSERT INTO auditoria (id, empresa, pregunta, respuesta, norma, fecha, auditor) VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?);",
-        [null, empresa, pregunta, respuesta, norma, auditor], function (err, result, fields) {
-            if (err) throw err;
-            if (result.affectedRows >= 1) {
-                res.json({ insert: true });
-            } else {
-                res.json({ insert: false });
-            }
-        });
-});
-app.get('/auditoriaresultado/get/:idempresa', function (req, res) {
-    var idempresa = req.params.idempresa;
-    var consulta = "select e.nombre, e.nit, n.nombre, p.texto, a.respuesta, u.usuario, a.fecha from auditoria a ";
-    consulta = consulta + "inner join empresa e on a.empresa = e.id ";
-    consulta = consulta + "inner join pregunta p on a.pregunta = p.id ";
-    consulta = consulta + "inner join norma n on a.norma = n.id ";
-    consulta = consulta + "inner join usuario u on a.auditor = u.id ";
-    consulta = consulta + "where e.id = ?;";
-    con.query(consulta, [idempresa], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
+// app.get('/auditoria/get', function (req, res) {
+//     con.query("SELECT * FROM auditoria", function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/auditoria/get/:id', function (req, res) {
+//     var id = req.params.id;
+//     con.query("SELECT * FROM auditoria WHERE id = ?", [id], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
 
-//empresa
-app.get('/empresa/get', function (req, res) {
-    con.query("SELECT * FROM empresa", function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/empresa/get/:id', function (req, res) {
-    var id = req.params.id;
-    con.query("SELECT * FROM empresa WHERE id = ?", [id], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/empresa/save/:name/:nit', function (req, res) {
-    var name = req.params.name;
-    var nit = req.params.nit;
-    con.query("INSERT INTO empresa (id, nombre, nit) VALUES (?, ?, ?);", [null, name, nit], function (err, result, fields) {
-        if (err) throw err;
-        if (result.affectedRows >= 1) {
-            res.json({ insert: true });
-        } else {
-            res.json({ insert: false });
-        }
-    });
-});
+// app.get('/auditoria/save/:empresa/:pregunta/:respuesta/:norma/:auditor', function (req, res) {
+//     var empresa = req.params.empresa;
+//     var pregunta = req.params.pregunta;
+//     var respuesta = req.params.respuesta;
+//     var norma = req.params.norma;
+//     var auditor = req.params.auditor;
+//     con.query("INSERT INTO auditoria (id, empresa, pregunta, respuesta, norma, fecha, auditor) VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?);",
+//         [null, empresa, pregunta, respuesta, norma, auditor], function (err, result, fields) {
+//             if (err) throw err;
+//             if (result.affectedRows >= 1) {
+//                 res.json({ insert: true });
+//             } else {
+//                 res.json({ insert: false });
+//             }
+//         });
+// });
+// app.get('/auditoriaresultado/get/:idempresa', function (req, res) {
+//     var idempresa = req.params.idempresa;
+//     var consulta = "select e.nombre, e.nit, n.nombre, p.texto, a.respuesta, u.usuario, a.fecha from auditoria a ";
+//     consulta = consulta + "inner join empresa e on a.empresa = e.id ";
+//     consulta = consulta + "inner join pregunta p on a.pregunta = p.id ";
+//     consulta = consulta + "inner join norma n on a.norma = n.id ";
+//     consulta = consulta + "inner join usuario u on a.auditor = u.id ";
+//     consulta = consulta + "where e.id = ?;";
+//     con.query(consulta, [idempresa], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
 
-//norma
-app.get('/norma/get', function (req, res) {
-    con.query("SELECT * FROM norma", function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/norma/get/:id', function (req, res) {
-    var id = req.params.id;
-    con.query("SELECT * FROM norma WHERE id = ?", [id], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/norma/save/:name', function (req, res) {
-    var name = req.params.name;
-    con.query("INSERT INTO norma (id, nombre) VALUES (?, ?);", [null, name], function (err, result, fields) {
-        if (err) throw err;
-        if (result.affectedRows >= 1) {
-            res.json({ insert: true });
-        } else {
-            res.json({ insert: false });
-        }
-    });
-});
+
+// app.get('/norma/get/:id', function (req, res) {
+//     var id = req.params.id;
+//     con.query("SELECT * FROM norma WHERE id = ?", [id], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/norma/save/:name', function (req, res) {
+//     var name = req.params.name;
+//     con.query("INSERT INTO norma (id, nombre) VALUES (?, ?);", [null, name], function (err, result, fields) {
+//         if (err) throw err;
+//         if (result.affectedRows >= 1) {
+//             res.json({ insert: true });
+//         } else {
+//             res.json({ insert: false });
+//         }
+//     });
+// });
+
+
 
 //preguntas
-app.get('/pregunta/get', function (req, res) {
-    con.query("SELECT * FROM pregunta", function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/pregunta/get/:id', function (req, res) {
-    var id = req.params.id;
-    con.query("SELECT * FROM pregunta WHERE id = ?", [id], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/preguntanorma/get/:idnorma', function (req, res) {
-    var idnorma = req.params.idnorma;
-    con.query("SELECT * FROM pregunta WHERE norma = ?", [idnorma], function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
-    });
-});
-app.get('/pregunta/save/:idnorma/:pregunta', function (req, res) {
-    var idnorma = req.params.idnorma;
-    var pregunta = req.params.pregunta;
-    con.query("INSERT INTO pregunta (id, norma, texto) VALUES (?, ?, ?);", [null, idnorma, pregunta], function (err, result, fields) {
-        if (err) throw err;
-        if (result.affectedRows >= 1) {
-            res.json({ insert: true });
-        } else {
-            res.json({ insert: false });
-        }
-    });
-});
+// app.get('/pregunta/get', function (req, res) {
+//     con.query("SELECT * FROM pregunta", function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/pregunta/get/:id', function (req, res) {
+//     var id = req.params.id;
+//     con.query("SELECT * FROM pregunta WHERE id = ?", [id], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/preguntanorma/get/:idnorma', function (req, res) {
+//     var idnorma = req.params.idnorma;
+//     con.query("SELECT * FROM pregunta WHERE norma = ?", [idnorma], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/pregunta/save/:idnorma/:pregunta', function (req, res) {
+//     var idnorma = req.params.idnorma;
+//     var pregunta = req.params.pregunta;
+//     con.query("INSERT INTO pregunta (id, norma, texto) VALUES (?, ?, ?);", [null, idnorma, pregunta], function (err, result, fields) {
+//         if (err) throw err;
+//         if (result.affectedRows >= 1) {
+//             res.json({ insert: true });
+//         } else {
+//             res.json({ insert: false });
+//         }
+//     });
+// });
 
 //usuario
 // app.get('/usuario/get/:id', function (req, res) {
@@ -295,15 +317,22 @@ app.get('/pregunta/save/:idnorma/:pregunta', function (req, res) {
 //     });
 // });
 
-app.listen(3000, function () {
-    console.log("Funciona 127.0.0.1:3000");
-});
-
-/*
-app.get("/", function (req, res){
-    res.send("Hola.");    
-});
-app.get("/test", function (req, res){
-    res.send("Test.");    
-});
-app.listen(3000, function (){console.log("Funciona");});*/
+// app.get('/empresa/get/:id', function (req, res) {
+//     var id = req.params.id;
+//     con.query("SELECT * FROM empresa WHERE id = ?", [id], function (err, result, fields) {
+//         if (err) throw err;
+//         res.json(result);
+//     });
+// });
+// app.get('/empresa/save/:name/:nit', function (req, res) {
+//     var name = req.params.name;
+//     var nit = req.params.nit;
+//     con.query("INSERT INTO empresa (id, nombre, nit) VALUES (?, ?, ?);", [null, name, nit], function (err, result, fields) {
+//         if (err) throw err;
+//         if (result.affectedRows >= 1) {
+//             res.json({ insert: true });
+//         } else {
+//             res.json({ insert: false });
+//         }
+//     });
+// });
